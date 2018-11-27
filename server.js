d20001ed24eb08de790769b7d5ef11c1c0001fdc
jamesbillard <jamesbillard12@hotmail.com>
2018-11-27 00:31:13 -0800

    able to creat wav file but voices is fuct

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const say = require("say");
 const Twit = require("twit");
+const fs = require("fs");
+
+var nodeoutlook = require("nodejs-nodemailer-outlook");
 require("dotenv").config();
 
 let T = new Twit({
@@ -9,21 +12,44 @@ let T = new Twit({
   access_token_secret: process.env.ACCESS_TOKEN_SECRET
 });
 
-// T.get(
-//   "statuses/user_timeline",
-//   { screen_name: "realDonaldTrump", count: 1, tweet_mode: "extended" },
-//   function(err, data, response) {
-//     console.log(data);
-//     say.speak(data[0].full_text);
-//   }
-// );
+let prevTweet;
+let count = 0;
 
-let stream = T.stream("statuses/filter", {
-  follow: "25073877",
-  tweet_mode: "extended"
-});
-stream.on("tweet", function(tweet) {
-  if (tweet.user.id === "25073877") {
-    if (tweet.extended_tweet) console.log(tweet.extended_tweet.full_text);
+// setInterval(() => {
+T.get(
+  "statuses/user_timeline",
+  { screen_name: "realDonaldTrump", count: 1, tweet_mode: "extended" },
+  function(err, data, response) {
+    count++;
+    console.log(count);
+    if (err) console.log(err);
+    if (prevTweet !== data[0].full_text) {
+      console.log("NEW TWEET");
+      console.log(data[0].full_text);
+      prevTweet = data[0].full_text;
+      // say.speak(data[0].full_text);
+      nodeoutlook.sendEmail({
+        auth: {
+          user: process.env.EMAIL,
+          pass: process.env.PASSWORD
+        },
+        from: process.env.EMAIL,
+        to: process.env.EMAIL,
+        subject: "New Trump Tweet!",
+        text: data[0].full_text
+      });
+      (async () => {
+        const text2wav = require("text2wav");
+        let out = await text2wav(data[0].full_text, {
+          voice: "en"
+        });
+        fs.appendFile("bb.wav", new Buffer(out), function(err) {});
+      })();
+    } else {
+      console.log("NO NEW TWEET");
+    }
   }
-});
+);
+// }, 60000);
+
+console.log("up and running");
